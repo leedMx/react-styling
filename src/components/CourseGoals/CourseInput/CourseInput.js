4f78ce69e2d04414522edb9e858fa14c39aa8f5a
leedMx <esparza.mexico@gmail.com>
2023-01-12 17:12:59 -0600

    Using backtick and dynamic class assigning

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -15,18 +15,16 @@ const CourseInput = props => {
 
   const formSubmitHandler = event => {
     event.preventDefault();
-    if (isValid){
+    if (isValid) {
       props.onAddGoal(enteredValue);
     }
   };
 
-  const inputStyle = {borderColor: !isValid ? 'red' : 'black', background: !isValid ? 'salmon' : 'transparent'};
-
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label style={{color: !isValid ? 'red' : 'black'}}>Course Goal</label>
-        <input style={inputStyle} type="text" onChange={goalInputChangeHandler} />
+      <div className={`form-control ${isValid ? '' : 'invalid'}`}>
+        <label>Course Goal</label>
+        <input type="text" onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
