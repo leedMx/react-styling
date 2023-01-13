b2a5fb5770962048166d8559e49b93916685de95
leedMx <esparza.mexico@gmail.com>
2023-01-12 19:11:06 -0600

    Using css module example

--- src/components/CourseGoals/CourseGoalList/CourseGoalList.js
+++ src/components/CourseGoals/CourseGoalList/CourseGoalList.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 import CourseGoalItem from '../CourseGoalItem/CourseGoalItem';
-import './CourseGoalList.css';
+import classes from './CourseGoalList.module.css';
 
 const CourseGoalList = props => {
   return (
-    <ul className="goal-list">
+    <ul className={classes['goal-list']}>
       {props.items.map(goal => (
         <CourseGoalItem
           key={goal.id}
